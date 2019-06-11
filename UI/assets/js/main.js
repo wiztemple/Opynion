b5c664591d17bb45177b1aec2ab0588f26f05d89
wiztemple <wiztemple@gmail.com>
2019-06-11 12:30:36 +0100

    new UI

--- UI/assets/js/main.js
+++ UI/assets/js/main.js
@@ -1,43 +1,22 @@
-const modal = document.querySelector('.modal');
-const modal2 = document.querySelector('.modal2');
-const modalBtn = document.getElementById('modalBtn');
-const detailsBtn = document.querySelector('.details-btn');
-const closeBtn = document.querySelector('.closeBtn');
+const commentBtns = document.querySelectorAll('.reaction');
+const inputFields = document.querySelectorAll('.inputField');
 
-function openModal() {
-  modal.style.display = 'block';
-}
 
-function openModal2() {
-  modal2.style.display = 'block';
-}
+[...commentBtns].map(commentBtn => {
+  commentBtn.addEventListener('click', () => {
 
-function closeModal() {
-  modal.style.display = 'none';
-}
-function eventListener() {
-  modalBtn.addEventListener('click', openModal);
-  detailsBtn.addEventListener('click', openModal2)
-  closeBtn.addEventListener('click', closeModal);
-}
+    [...inputFields].map(inputField => {
+      if (inputField.style.display === 'block') {
+        
+        inputField.style.display = 'none';
 
-eventListener();
+      } else {
+        inputField.style.display = 'block';
 
-// credit to Tommy Hodgins for this function
+      }
 
-document.addEventListener('input', (event) => {
-  if (event.target.tagName.toLowerCase() !== 'textarea') return;
-  autoIncrease(event.target);
-}, false)
 
-const autoIncrease = (field) => {
-  // Reset field height
-  field.style.height = 'inherit';
-  let computed = window.getComputedStyle(field);
-  // Calculate the height
-  let height = parseInt(computed.getPropertyValue('border-top-width'), 10)
-    + parseInt(computed.getPropertyValue('padding-top'), 10) + field.scrollHeight
-    + parseInt(computed.getPropertyValue('padding-bottom'), 10)
-    + parseInt(computed.getPropertyValue('border-bottom-width'), 10);
-  field.style.height = height + 'px';
-}
+    });
+
+  });
+});
--- src/pages/SignUp/interfaces.d.ts
+++
@@ -1,7 +0,0 @@
-export interface SignUpState {
-  firstName: string,
-  lastName: string,
-  email: string,
-  username: string,
-  password: string
-}
