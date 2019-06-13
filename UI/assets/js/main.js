5a5b9ab867d302a1a2941f46aa8e3ffa50996304
wiztemple <wiztemple@gmail.com>
2019-06-13 17:31:47 +0100

    add profile page

--- UI/assets/js/main.js
+++ UI/assets/js/main.js
@@ -7,7 +7,7 @@ const inputFields = document.querySelectorAll('.inputField');
 
     [...inputFields].map(inputField => {
       if (inputField.style.display === 'block') {
-        
+
         inputField.style.display = 'none';
 
       } else {
@@ -20,3 +20,20 @@ const inputFields = document.querySelectorAll('.inputField');
 
   });
 });
+
+// tabs
+// const tabjs = document.querySelectorAll('.tabjs');
+const openTab = (event, tabName) => {
+  let tabcontent = document.querySelectorAll('.tabcontent');
+  [...tabcontent].map(tab => tab.style.display = "none");
+  let tablinks = document.querySelectorAll(".tablinks");
+  [...tablinks].map(tablink => tablink.className = tablink.className.replace(" active", ""));
+  document.getElementById(tabName).style.display = "block";
+  event.currentTarget.className += " active";
+}
+// [...tabjs].map(tablink => {
+//   console.log(tablink, 'tablink');
+
+//   return tablink.addEventListener('click', openTab
+//   )
+// })
