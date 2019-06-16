3c5e975628775774ec03088252569b9740c42a50
wiztemple <wiztemple@gmail.com>
2019-06-16 18:05:15 +0100

    make modals functional and add signup modal

--- UI/assets/js/main.js
+++ UI/assets/js/main.js
@@ -1,5 +1,9 @@
 const commentBtns = document.querySelectorAll('.reaction');
 const inputFields = document.querySelectorAll('.inputField');
+const signup = document.querySelector('.signup');
+const modals = document.querySelectorAll('.modal');
+const cancelBtn = document.querySelectorAll('.cancel-btn');
+const authLinks = document.querySelectorAll('.authLink');
 
 
 [...commentBtns].map(commentBtn => {
@@ -31,9 +35,44 @@ const openTab = (event, tabName) => {
   document.getElementById(tabName).style.display = "block";
   event.currentTarget.className += " active";
 }
+/**
+ * Display the signup form
+ *
+ * @param {null}
+ *
+ * @returns signup modal
+ */
+const displaySignUp = () => signup.style.display = 'flex';
+
+[...authLinks].map(authLink => {
+  authLink.addEventListener('click', displaySignUp);
+});
+/**
+ * Makes the closeIcon re-usable
+ *
+ * @param {null}
+ *
+ * @returns
+ */
+  
+[...cancelBtn].map((cancel) => {
+  cancel.addEventListener('click', () => {
+    const divToClose = cancel.closest('.target-js');
+    divToClose.style.display = 'none';
+  });
+});
+
 // [...tabjs].map(tablink => {
 //   console.log(tablink, 'tablink');
 
 //   return tablink.addEventListener('click', openTab
 //   )
 // })
+// const navbar = document.getElementById('fixbar');
+// window.onscroll = function () {
+//   if (window.pageYOffset >= 100) {
+//     navbar.style.boxShadow = 'rgba(17, 17, 18, 0.04) 0 2px 4px, rgba(19, 20, 20, 0.06) 0 1px 1px';
+//   } else {
+//     navbar.style.boxShadow = '0px';
+//   }
+// };
